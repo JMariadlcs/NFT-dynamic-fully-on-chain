c3e494a2f2bc9849adc8c27ae255763402b53b38
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-04-30 01:01:50 +0200

    Contract

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -2,7 +2,28 @@
 
 pragma solidity ^0.8.7;
 
-contract DynamicSvgNft {
+    import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+    /**
+    * @dev
+    * 1. Mint an NFT based on the price of ETH
+    * - If ETHprice  > someNumer: NFT1
+    * - if ETHprice < someNumber: NFT2
+    */
+contract DynamicSvgNft is ERC721 {
+
+    uint256 public s_tokenCounter; // NFTid
+
+    constructor() ERC721("Dynamic SVG NFT", "DSN") {
+
+    }
+
+    function mintNft() external {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter +1; // update NFTid
+    }
+
 
 
 }
+
